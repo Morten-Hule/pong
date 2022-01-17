e2226821e55c530c5b6d27b17b977ddb7824b56c
mortenlove <55623201+Morten-Hule@users.noreply.github.com>
2022-01-17 20:14:27 +0300

    1

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (request, response) => {
   var clientIp = requestIp.getClientIp(request);
   console.log(clientIp);
-  response.sendFile(`${__dirname}/public/indexpong.html`);
+  response.sendFile(`${__dirname}/public/index.html`);
 });
 app.listen(port, () => {
     console.log(`Application listening on port ${port}`);
