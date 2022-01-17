b43bbe38e02cabca56d6f654214efcc369575678
mortenlove <55623201+Morten-Hule@users.noreply.github.com>
2022-01-17 20:11:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 80
+const winston = require("winston");
+var requestIp = require('request-ip');
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (request, response) => {
+  var clientIp = requestIp.getClientIp(request);
+  console.log(clientIp);
+  response.sendFile(`${__dirname}/public/indexpong.html`);
+});
+app.listen(port, () => {
+    console.log(`Application listening on port ${port}`);
+});
+
